7d23b3f6ade478db1870ad8e764b352811fcf403
jjhlzn <jinjunhang@hotmail.com>
2016-05-18 00:21:05 +0800

    集成消息通知

---
+++ Server/App_Code/MDUtils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+    /// <summary>
+    /// MD5Util 的摘要说明。
+    /// </summary>
+    public class MD5Util
+    {
+        public MD5Util()
+        {
+            //
+            // TODO: 在此处添加构造函数逻辑
+            //
+        }
+
+        /** 获取大写的MD5签名结果 */
+        public static string GetMD5(string encypStr, string charset)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(encypStr);
+            byte[] md5data = md5.ComputeHash(data);
+            md5.Clear();
+            string str = "";
+            for (int i = 0; i < md5data.Length; i++)
+            {
+                str += md5data[i].ToString("x").PadLeft(2, '0');
+            }
+            var result = str.ToLower();
+            return result;
+        }
+
+        //string retStr;
+        //MD5CryptoServiceProvider m5 = new MD5CryptoServiceProvider();
+
+        ////创建md5对象
+        //byte[] inputBye;
+        //byte[] outputBye;
+
+        ////使用GB2312编码方式把字符串转化为字节数组．
+        //try
+        //{
+        //    inputBye = Encoding.GetEncoding(charset).GetBytes(encypStr);
+        //}
+        //catch (Exception ex)
+        //{
+        //    inputBye = Encoding.GetEncoding("UTF-8").GetBytes(encypStr);
+        //}
+        //outputBye = m5.ComputeHash(inputBye);
+
+        //retStr = System.BitConverter.ToString(outputBye);
+        //retStr = retStr.Replace("-", "").ToUpper();
+        //return retStr;
+        //}
+    }
+
---
+++ Server/sendNotify.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using log4net;
+
+public partial class sendNotify : System.Web.UI.Page
+{
+    private ILog Logger = LogManager.GetLogger(typeof (sendNotify));
+    private String serviceUrl = "openapi.xg.qq.com/v2/push/single_device";
+     
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var url = "http://" + serviceUrl;
+
+        Hashtable parameters = new Hashtable();
+        parameters["access_id"] = "2200199196";
+        parameters["timestamp"] = ConvertDateTimeInt(DateTime.Now).ToString();
+       
+
+        var message = "{\"aps\":{\"alert\":\"gogogo\"}}";
+        //var message = "{}";
+        parameters["message"] = message;
+        parameters["message_type"] = "0";
+        parameters["environment"] = "2";
+        parameters["device_token"] = "1c2a875088c8a6c23959aa72468357d1eb6a8d6f3297da405a5b6664ef489250";
+
+        parameters["sign"] = createSign(serviceUrl, parameters);
+
+        var postData = GetPostData(parameters);
+
+        var responseString = OpenReadWithHttps(url, postData);
+
+        Logger.Debug("response = " + responseString);
+        Response.Write(responseString);
+
+        
+    }
+
+    public string OpenReadWithHttps(string URL, string strPostdata)
+    {
+        Encoding encoding = Encoding.UTF8;
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+        request.Method = "post";
+        request.Accept = "text/html, application/xhtml+xml, */*";
+        request.ContentType = "application/x-www-form-urlencoded";
+        byte[] buffer = encoding.GetBytes(strPostdata);
+        request.ContentLength = buffer.Length;
+        request.GetRequestStream().Write(buffer, 0, buffer.Length);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        using (StreamReader reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+
+    public int ConvertDateTimeInt(System.DateTime time)
+    {
+        double intResult = 0;
+        System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+        intResult = (time - startTime).TotalSeconds;
+        return (int)intResult;
+    }
+
+    public String createSign(String url, Hashtable parameters)
+    {
+        StringBuilder sb = new StringBuilder("POST"+url);
+
+        ArrayList akeys = new ArrayList(parameters.Keys);
+        akeys.Sort();
+
+        foreach (string k in akeys)
+        {
+            string v = (string)parameters[k];
+            if (null != v && "".CompareTo(v) != 0
+                && "sign".CompareTo(k) != 0 && "key".CompareTo(k) != 0)
+            {
+                sb.Append(k + "=" + v);
+            }
+        }
+
+        sb.Append(this.getKey());
+        Logger.Debug(sb.ToString());
+        string sign = MD5Util.GetMD5(sb.ToString(), "UTF-8");
+        Logger.Debug("sign=  " + sign);
+        return sign;
+    }
+
+    public String GetPostData(Hashtable parameters)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        ArrayList akeys = new ArrayList(parameters.Keys);
+        akeys.Sort();
+
+        foreach (string k in akeys)
+        {
+            string v = (string)parameters[k];
+
+            sb.Append(k + "=" + v + "&");
+            
+        }
+        var result = sb.ToString().Substring(0, sb.Length - 1);
+        Logger.Debug(result);
+        return result;
+    }
+
+    private String getKey()
+    {
+        return "3ba8903b6c3ae3b74cf2eebedfcc6188";
+
+    }
+     
+}
